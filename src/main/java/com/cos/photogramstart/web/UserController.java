373273d8f094d49266b40bcc3f3b3819761b9096
mvcbohyun <jbh959847@gmail.com>
2021-06-14 14:37:32 +0900

    유저 세션정보 확인 !

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,8 +1,13 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDtails;
 
 @Controller
 public class UserController {
@@ -12,7 +17,14 @@ public class UserController {
 		return "user/profile";
 	}
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable Long id) {
+	public String update(@PathVariable Long id, @AuthenticationPrincipal PrincipalDtails principalDtails) {
+		// 이걸로 찾으면 세션을 쉽게 찾을수 있음
+		System.out.println("세션정보 : "+ principalDtails.getUser());
+		
+		//이렇게도 찾을수 있음 사용하지 말것 !
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();	
+		PrincipalDtails mPrincipalDtails  = (PrincipalDtails)auth.getPrincipal();
+		System.out.println("직접 찾은 세션정보 : "+mPrincipalDtails.getUser());
 		return "user/update";
 	}
 }
