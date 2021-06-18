439a0c7b828a81f1886f17094858d0182d29f46e
mvcbohyun <jbh959847@gmail.com>
2021-06-18 11:51:20 +0900

    이미지 도메인 , 리포지토리 생성

---
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -0,0 +1,50 @@
+package com.cos.photogramstart.domain.image;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Image {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)//번호 증가 전략이 데이터 베이스를 따라간다.
+	private Long id;
+	
+	private String caption;// 사진 내용 
+	private String postImageUrl;// 사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장 -DB에 그 저장된 경로를 insert
+	@JoinColumn(
+			name="userid"
+			)
+	@ManyToOne
+	private User user;
+	
+	//  이미지 좋아요
+	
+	//  댓글
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist// 디비에 insert 되기 직전에 실행
+	public void createDate() {
+		this.createDate= LocalDateTime.now();
+	}
+	
+	
+
+}
---
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.cos.photogramstart.domain.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ImageRepository  extends JpaRepository<Image, Integer> {
+
+}
