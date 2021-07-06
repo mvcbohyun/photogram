c0a3cc937562a3c28b9c96f28ac2798365c9dca3
mvcbohyun <jbh959847@gmail.com>
2021-07-06 11:55:30 +0900

    댓글달기 api 생성

--- src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
+++ src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.cos.photogramstart.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
+// 네이티브 쿼리는 int만 리턴 받을수 있음
+//	@Modifying
+//	@Query(value = "INSERT INTO comment(content,imageId,userId,createDate) VALUES(:content,:imageId,:userId,now())",nativeQuery = true)
+//	Comment mSave(String content,Long imageId,Long userId);
 }
--- src/main/java/com/cos/photogramstart/service/CommentService.java
+++ src/main/java/com/cos/photogramstart/service/CommentService.java
@@ -5,6 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.comment.Comment;
 import com.cos.photogramstart.domain.comment.CommentRepository;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +18,26 @@ import lombok.RequiredArgsConstructor;
 public class CommentService {
 
 	private final CommentRepository commentRepository;
+	private final UserRepository userRepository;
 	
 	@Transactional
-	public Comment 댓글쓰기() {
+	public Comment 댓글쓰기(String content, Long imageId, Long userId) {
+		//tip (객체를 만들때 아이디 값만 담아서 insert 할 수 있다 . -- 이렇게 안하면 다 찾아서 진행해야 해서 복잡스)
+		// 대신 return 시에 완벽한 객체를 받고 싶으면 이렇게 하면 안됨!
+		//image 객체와 user 객체는 id 값만 가지고 있는 빈 객체를 리턴받음.
+		Image image = new Image();
+		image.setId(imageId);
+		User userEntity = userRepository.findById(userId);
 		
-		return null;
+		if(userEntity==null) {
+			throw new CustomApiException("찾을수 없는 유저입니다.");
+		}
+		
+		Comment comment = new Comment();
+		comment.setContent(content);
+		comment.setImage(image);
+		comment.setUser(userEntity);
+		return commentRepository.save(comment);
 	}
 	
 	@Transactional
